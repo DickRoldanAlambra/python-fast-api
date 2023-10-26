700e7213b627b48d846ad06cb875f0b3b14af61a
John Eric Magat <johnericmagat@gmail.com>
2023-10-26 14:25:17 +0800

    Code review

--- Controllers/MstPayTypeController.py
+++ Controllers/MstPayTypeController.py
@@ -6,9 +6,8 @@ session = SessionLocal()
 router = APIRouter()
 
 @router.get('/paytypes')
-async def get_all_paytypes():
+async def all():
     try:
-      # Get all paytypes instances and join them with their associated accounts
         paytypes = (
             session.query(Models.MstPayTypeModel.PayType, Models.MstAccountModel.Account)
             .join(Models.MstAccountModel.Account, Models.MstPayTypeModel.PayType.AccountId == Models.MstAccountModel.Account.Id)
@@ -27,6 +26,5 @@ async def get_all_paytypes():
 
         return paytype_account_data
     except Exception as e:
-        # Handle any potential errors, such as database exceptions
-        session.rollback()  # Roll back the transaction
+        session.rollback()
         raise HTTPException(status_code=500, detail="Internal server error")
