59a8d7edf7fd57dd083b4b7def8b0a9a3333aed8
John Eric Magat <johnericmagat@gmail.com>
2023-10-26 13:09:07 +0800

    Update connection string

--- initializer.py
+++ initializer.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
-from sqlalchemy import create_engine, Column, DateTime, Integer, String, Boolean, ForeignKey
+from sqlalchemy import create_engine, URL, Column, DateTime, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 from decimal import Decimal
 from pydantic import BaseModel
@@ -17,10 +17,12 @@ db_database = os.getenv("DB_DATABASE")
 db_username = os.getenv("DB_USERNAME")
 db_password = os.getenv("DB_PASSWORD")
 
-connection_string = f"mssql+pyodbc://{db_username}:{db_password}@{db_server}/{db_database}?driver=SQL Server"
+connection_string = "DRIVER={SQL Server};SERVER=localhost;DATABASE=easypos;UID=sa;PWD=P@ssword1234"
+connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+
 # engine = create_engine(connection_string)
 # Debugging
-engine = create_engine(connection_string, echo=True)
+engine = create_engine(connection_url, echo=True)
 #This resolves issues with bulk inserts or operations:
 connection = engine.raw_connection()
 connection.cursor().fast_executemany = True
